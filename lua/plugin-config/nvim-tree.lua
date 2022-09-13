aecfe17e9308287def732aac0d5d559108cd0b9c
jane <74652705+jane-212@users.noreply.github.com>
2022-09-13 19:38:54 +0800

    Update nvim-tree.lua

--- lua/plugin-config/nvim-tree.lua
+++ lua/plugin-config/nvim-tree.lua
@@ -2,5 +2,10 @@ require('nvim-tree').setup{
     view = {
       hide_root_folder = true,
       signcolumn = "no",
+      mappings = {
+            list = {
+                { key = "<C-k>", action = "" },
+            },
+      },
     }, 
 }
