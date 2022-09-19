3e7d5810ed14e0e5d0c3d9a36a86e32d9255cbca
jane <jjt1525@outlook.com>
2022-09-19 18:21:03 +0800

    edit basic.lua

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,18 +1,18 @@
 vim.cmd([[
 colorscheme gruvbox
 set expandtab
-set tabstop = 4
-set shiftwidth = 4
+set tabstop=4
+set shiftwidth=4
 set relativenumber
 set number
-set t_Co = 256
-set background = dark
+set t_Co=256
+set background=dark
 set termguicolors
-set shortmess += c
-set updatetime = 100
-set encoding = utf-8
+set shortmess+=c
+set updatetime=100
+set encoding=utf-8
 set nobackup
 set noswapfile
 set noundofile
-set signcolumn = no
+set signcolumn=no
 ]])
