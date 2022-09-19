8fa4cacdc677a7bd0e998b50301b183083ba3a73
jane <jjt1525@outlook.com>
2022-09-19 17:45:43 +0800

    edit basic.lua

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,16 +1,19 @@
-vim.opt.expandtab = true
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.relativenumber = true
-vim.opt.number = true
-vim.opt.termguicolors = true
-vim.opt.ignorecase = true
-vim.opt.termguicolors = true
-vim.shortmess:append "c"
-vim.opt.updatetime = 100
-vim.opt.encoding = "utf-8"
-vim.opt.nobackup = true
-vim.opt.noswapfile = true
-vim.opt.noundofile = true
-vim.opt.signcolumn = "no"
+vim.cmd([[
+colorscheme gruvbox
+set background = dark
+set rainbow_active = 1
+set expandtab
+set tabstop = 4
+set shiftwidth = 4
+set relativenumber
+set number
+set t_Co = 256
+set termguicolors
+set shortmess += c
+set updatetime = 100
+set encoding = utf-8
+set nobackup
+set noswapfile
+set noundofile
+set signcolumn = no
+]])
